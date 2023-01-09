75eff00caa431b1371a6b76884a5a751cc62ccc8
Mohamad <mohamad.webdev@gmail.com>
2023-01-09 13:38:16 +0100

    WIP...

---
+++ howto.js
@@ -0,0 +1,24 @@
+/**
+ * Create a rout for your App
+ */
+//  Inside the routes directory create a new js file and  call it f.e myapp.js
+//  Copy the test.js to your new cretaed file and chenage it according to your App routes.
+
+// Creat a view :
+// Create a dir for your app inside the frontend/source/apps. Call it f.e myapp
+// Myapp dir must have this folder html, js. And this files: config.json and index.scss
+// Your JS files will exist  inside the js folder and your html files will exist  inside the html floder
+
+// The myapp/html/main.ejs file will be the entry point for your html code.
+// The myapp/js/inde.js file will be the entry point for your js files
+// The myapp/index.scss file will be the entry point for your scss files
+
+// First time :
+// Run npm run dev --app_name=myapp for localy deploi=ying your app.
+// Run node index.js
+// Go to localhost:300/myapp
+
+// Then:
+// Every time yo change the frontend/source you have to run npm run dev --app_name=myapp to get changes.
+
+// NODEJS, EXPRESSJS and EJS template
--- routes/myapp.js
+++ routes/myapp.js
@@ -10,21 +10,21 @@ const router = require('express').Router();
 // })
 
 
-/// This will your main route - www.example.com/test/
+/// This will your main route - www.example.com/myapp
 router.get('/', ( p_req, p_res)=>{
     
-    view( p_res, 'test', 'test', 'default');
+    view( p_res, 'main', 'main', 'default', {});
 });
 
 
-///  www.example.com/test/test1
-router.get('/test1', ( p_req, p_res)=>{
+///  www.example.com/myapp/test1
+router.get('/moha', ( p_req, p_res)=>{
    
     p_res.json({"route": "test1 "});
 });
 
 
-/// A post request - www.example.com/test/name
+/// A post request - www.example.com/myapp/name
 router.post('/name', ( p_req, p_res)=>{
    
     p_res.json({"name": "Moha"});
@@ -34,4 +34,4 @@ router.post('/name', ( p_req, p_res)=>{
 
 
 /// The name of your main route;
-app.use('/test', router );
+app.use('/myapp', router );
