c983013ce762c19b03d18cb55959f6f97c205f4b
Mohamad <mohamad.webdev@gmail.com>
2023-02-01 14:03:53 +0100

    Use JQ to subscribe for events.

--- frontend/public/apps/bookmeapp/index.js
+++ frontend/public/apps/bookmeapp/index.js
@@ -13,12 +13,23 @@ alert('BOOKMEAPP!');
 function openForm() {
   //document.getElementById("popupForm").style.display = "block";
   $("#popupForm").css("display", "block");
-}
+} /// Issue 10747
+/// Subscribe to an event form a javascript file to open the forme
+
+
+$('#forbusiness').on('click', function () {
+  $("#popupForm").css("display", "block");
+});
 
 function closeForm() {
   //document.getElementById("popupForm").style.display = "none";
   $("#popupForm").css("display", "none");
-}
+} /// Subscribe to an event form a javascript file to close the form
+
+
+$('.close-forme').on('click', function () {
+  $("#popupForm").css("display", "none");
+});
 
 /***/ }),
 
--- frontend/source/apps/bookmeapp/js/index.js
+++ frontend/source/apps/bookmeapp/js/index.js
@@ -6,9 +6,25 @@ function openForm() {
     $("#popupForm").css("display","block");
 }
 
+/// Issue 10747
+
+/// Subscribe to an event form a javascript file to open the forme
+$('#forbusiness').on('click', function(){
+    $("#popupForm").css("display","block");
+})
+
+
+
+
 function closeForm() {
     //document.getElementById("popupForm").style.display = "none";
     $("#popupForm").css("display","none");
     
 }
+
+
+/// Subscribe to an event form a javascript file to close the form
+$('.close-forme').on('click', function(){
+    $("#popupForm").css("display","none");
+})
 
