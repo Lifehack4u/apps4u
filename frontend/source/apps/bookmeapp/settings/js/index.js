2b44f354d8703c19cd555d7c2af010741dec8f2a
33lacru <larissa.sia@gmail.com>
2023-02-11 18:23:18 +0100

    feb11

--- frontend/source/apps/bookmeapp/settings/js/index.js
+++ frontend/source/apps/bookmeapp/settings/js/index.js
@@ -1,2 +1,9 @@
 console.log('This is settings');
 alert('settings!');
+
+var test=[];
+$('#testsettingsbutton').on('click', function(){
+    alert("hello");
+   test.push({"test": "settings"});
+})
+
